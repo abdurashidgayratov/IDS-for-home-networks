69d82055cd2815b06865d2e4fd69f3b6606ef2a9
pdP <abdurashid.gayratov@idu.uz>
2026-01-16 03:33:28 +0500

    Update documentation to English with author info

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://cibarian-carylon-stealthily.ngrok-free.dev';
+const API_URL = 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_URL,
